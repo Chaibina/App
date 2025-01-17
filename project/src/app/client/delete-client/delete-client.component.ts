88ee6c6d8311a188a83aa9fc5ecfc97893c5380f
Aida <aidacheybaineh@gmail.com>
2025-01-17 10:50:29 +0100

    client Module

---
+++ project/src/app/client/delete-client/delete-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-client',
+  imports: [],
+  templateUrl: './delete-client.component.html',
+  styleUrl: './delete-client.component.css'
+})
+export class DeleteClientComponent {
+
+}
